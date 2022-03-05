34c72af29e322441a296763117749d859f9b355f
kynail <kylian.gence@epitech.eu>
2022-03-05 14:05:03 +0400

    modif

--- Assets/AreneGame/Scripts/GameMode/GameMode.cs
+++ Assets/AreneGame/Scripts/GameMode/GameMode.cs
@@ -5,17 +5,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// A gamemode class network object, owned and instantiated by the server
-/// </summary>
 public class GameMode : GameModeBehavior
 {
-
-
-    // Use this for initialization
     void Start()
     {
-        //safety check lol
         if (!networkObject.IsServer)
         {
             return;
@@ -25,17 +18,12 @@ public class GameMode : GameModeBehavior
         {
             MainThreadManager.Run(() =>
             {
-                //Do some counting logic here for a gamemode, eg, assign team to newly joined player, or restart round if enough people joined
-                //Remember to remove players from counter in playerDisconnected event as well
             });
         };
-
-        //Handle disconnection
         NetworkManager.Instance.Networker.playerDisconnected += (player, sender) =>
         {
             MainThreadManager.Run(() =>
             {
-                //Loop through all players and find the player who disconnected, store all it's networkobjects to a list
                 List<NetworkObject> toDelete = new List<NetworkObject>();
                 foreach (var no in sender.NetworkObjectList)
                 {
@@ -45,8 +33,6 @@ public class GameMode : GameModeBehavior
                         toDelete.Add(no);
                     }
                 }
-
-                //Remove the actual network object outside of the foreach loop, as we would modify the collection at runtime elsewise. (could also use a return, too late)
                 if (toDelete.Count > 0)
                 {
                     for (int i = toDelete.Count - 1; i >= 0; i--)
--- Assets/AreneGame/Scripts/Player/WeaponSystem/ProjectileOverrides/RPGProjectile.cs
+++ Assets/AreneGame/Scripts/Player/WeaponSystem/ProjectileOverrides/RPGProjectile.cs
@@ -3,28 +3,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// A version of the BaseProjectile used for the RPG projectile
-/// </summary>
 public class RPGProjectile : BaseProjectile
 {
-    //Radius of the explosion damage
     [SerializeField]
     private float radius = 3;
 
-    //The particle to play on impact
     [SerializeField]
     private ParticleSystem impactParticle;
 
-    // Use this for initialization
     public override void Start()
     {
         base.Start();
     }
 
-    /// <summary>
-    /// When the projectile has reached it's target, for calculation check out BaseProjectile class
-    /// </summary>
     public override void OnReachedTarget()
     {
         base.OnReachedTarget();
@@ -34,13 +25,10 @@ public class RPGProjectile : BaseProjectile
             impactParticle.Play();
         }
 
-        //hide the mesh
         GetComponent<MeshRenderer>().enabled = false;
 
-        //Destroy the gameobject after 2 seconds to make sure particle has played
         Destroy(gameObject, 2);
 
-        //Do the explosion damage on the server only
         if (NetworkManager.Instance.IsServer)
         {
             Collider[] hitColliders = Physics.OverlapSphere(transform.position, radius);
@@ -49,22 +37,10 @@ public class RPGProjectile : BaseProjectile
                 HealthSystem hp = hitCollider.GetComponent<HealthSystem>();
                 if (hp)
                 {
-                    //calculate the actual damage based on the distance within the sphere (damage dropoff)
                     float actualDamage = damage - (Vector3.Distance(hp.transform.position, transform.position) * 10);
-                    //call the take damage method
                     hp.TakeDamage((int)actualDamage, attackerName,target,hitNormal);
                 }
             }
         }
     }
-
-    /// <summary>
-    /// Uncomment this if you want to debug/visualize the explosion damage sphere
-    /// </summary>
-    //void OnDrawGizmos()
-    //{
-    //    Gizmos.color = Color.red;
-    //    Gizmos.DrawWireSphere(transform.position, radius);
-    //}
-
 }
